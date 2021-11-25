8ba0bbc72fce3508a2b668c9becd0ea4c63aa192
iamidlek <iamidlek@naver.com>
2021-11-25 14:34:24 +0900

    2753

---
+++ KDTsol/if문/2753.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const inputData = fs.readFileSync("/dev/stdin").toString();
+
+if ((inputData % 4 === 0 && inputData % 100 !== 0) || inputData % 400 === 0) {
+  console.log(1);
+} else {
+  console.log(0);
+}

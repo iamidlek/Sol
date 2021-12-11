b22afadaba858a794f11e1f69b9077c31e000f2f
iamidlek <iamidlek@naver.com>
2021-12-11 21:24:26 +0900

    이진탐색도구(범위)

---
+++ KDT알고리즘/binarySearch/이진탐색도구(범위).js
@@ -0,0 +1,15 @@
+// 값이 [leftValue, rightValue]인 데이터의 개수를 반환하는 함수
+function countByRange(arr, leftValue, rightValue) {
+  // 유의: lowerBound와 upperBound는 end 변수의 값을 배열의 길이로 설정
+  let rightIndex = upperBound(arr, rightValue, 0, arr.length);
+  let leftIndex = lowerBound(arr, leftValue, 0, arr.length);
+  return rightIndex - leftIndex;
+}
+// 배열 선언
+let arr = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9];
+
+// 값이 4인 데이터 개수 출력
+console.log(countByRange(arr, 4, 4)); // 2
+
+// 값이 [-1, 3] 범위에 있는 데이터 개수 출력
+console.log(countByRange(arr, -1, 3)); // 6
